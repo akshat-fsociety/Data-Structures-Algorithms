2ad4e586ed250d618ba0acc888a172e4f42750c0
Akshat <64137417+akshat-fsociety@users.noreply.github.com>
2020-09-18 19:24:25 +0530

    Create Frequency.java

---
+++ Hash/Frequency.java
@@ -0,0 +1,38 @@
+import java.util.*; 
+class GFG  
+{  
+static void countFreq(int arr[], int n) 
+{ 
+    HashMap<Integer,  
+            Integer>mp = new HashMap<Integer,  
+                                     Integer>(); 
+                                       
+    // Traverse through array elements and 
+    // count frequencies 
+    for (int i = 0 ; i < n; i++) 
+    { 
+        if(mp.containsKey(arr[i])) 
+        { 
+            mp.put(arr[i], mp.get(arr[i]) + 1); 
+        } 
+        else
+        { 
+            mp.put(arr[i], 1); 
+        } 
+    } 
+      
+    // Traverse through map and print frequencies 
+    for (Map.Entry<Integer, 
+                   Integer> entry : mp.entrySet())  
+        System.out.println(entry.getKey() + " " +  
+                           entry.getValue()); 
+} 
+  
+// Driver Code 
+public static void main(String[] args)  
+{ 
+    int arr[] = { 10, 20, 20, 10, 10, 20, 5, 20 }; 
+    int n = arr.length; 
+    countFreq(arr, n);     
+} 
+} 
