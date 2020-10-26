a71db20307ebef668e6f026170f966977a7369fe
Akshat <64137417+akshat-fsociety@users.noreply.github.com>
2020-10-26 15:24:28 +0530

    Create 450A.java

---
+++ Codeforces/codeforces-ladders/Ladder A/450A.java
@@ -0,0 +1,88 @@
+//https://codeforces.com/problemset/problem/450/A
+
+// import java.io.*;
+// import java.util.*;
+import java.io.BufferedReader; 
+import java.io.IOException; 
+import java.io.InputStreamReader; 
+// import java.util.Scanner; 
+import java.util.StringTokenizer; 
+  
+public class jzzhu
+{ 
+    static class FastReader 
+    { 
+        BufferedReader br; 
+        StringTokenizer st; 
+  
+        public FastReader() 
+        { 
+            br = new BufferedReader(new
+                     InputStreamReader(System.in)); 
+        } 
+  
+        String next() 
+        { 
+            while (st == null || !st.hasMoreElements()) 
+            { 
+                try
+                { 
+                    st = new StringTokenizer(br.readLine()); 
+                } 
+                catch (IOException  e) 
+                { 
+                    e.printStackTrace(); 
+                } 
+            } 
+            return st.nextToken(); 
+        } 
+  
+        int nextInt() 
+        { 
+            return Integer.parseInt(next()); 
+        } 
+  
+        long nextLong() 
+        { 
+            return Long.parseLong(next()); 
+        } 
+  
+        double nextDouble() 
+        { 
+            return Double.parseDouble(next()); 
+        } 
+  
+        String nextLine() 
+        { 
+            String str = ""; 
+            try
+            { 
+                str = br.readLine(); 
+            } 
+            catch (IOException e) 
+            { 
+                e.printStackTrace(); 
+            } 
+            return str; 
+        } 
+    } 
+  
+    public static void main(String[] args) 
+    { 
+        FastReader s=new FastReader(); 
+        int n = s.nextInt();
+        int m = s.nextInt();
+        double a;
+        double max = Integer.MIN_VALUE;
+        int index=0;
+        for (int i = 1; i <= n; i++) {
+            a = s.nextDouble();
+            double ceil = Math.ceil(a/m);
+            if(ceil>=max){
+                max=ceil;
+                index=i;
+            }
+        }
+        System.out.println(index);
+    }
+} 
