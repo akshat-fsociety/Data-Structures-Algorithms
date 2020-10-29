abe620024bb122ca53b9fdb9ac3fdd6e672e36bf
Akshat <64137417+akshat-fsociety@users.noreply.github.com>
2020-10-29 19:18:43 +0530

    Create watermelon(A).java

---
+++ Codeforces/codeforces-ladders/Ladder A/watermelon(A).java
@@ -0,0 +1,86 @@
+//https://codeforces.com/contest/4/problem/A
+
+// import java.io.*;
+// import java.util.*;
+import java.io.BufferedReader; 
+import java.io.IOException; 
+import java.io.InputStreamReader; 
+// import java.util.Scanner; 
+import java.util.StringTokenizer; 
+  
+public class watermelon 
+{ 
+    static class FastReader 
+    { 
+        BufferedReader br; 
+        StringTokenizer st; 
+  
+        public FastReader() 
+        { 
+            br = new BufferedReader(new
+                     InputStreamReader(System.in)); 
+        } 
+  
+        String next() 
+        { 
+            while (st == null || !st.hasMoreElements()) 
+            { 
+                try
+                { 
+                    st = new StringTokenizer(br.readLine()); 
+                } 
+                catch (IOException  e) 
+                { 
+                    e.printStackTrace(); 
+                } 
+            } 
+            return st.nextToken(); 
+        } 
+  
+        int nextInt() 
+        { 
+            return Integer.parseInt(next()); 
+        } 
+  
+        long nextLong() 
+        { 
+            return Long.parseLong(next()); 
+        } 
+  
+        double nextDouble() 
+        { 
+            return Double.parseDouble(next()); 
+        } 
+  
+        String nextLine() 
+        { 
+            String str = ""; 
+            try
+            { 
+                str = br.readLine(); 
+            } 
+            catch (IOException e) 
+            { 
+                e.printStackTrace(); 
+            } 
+            return str; 
+        } 
+    } 
+  
+    public static void main(String[] args) 
+    { 
+        FastReader s=new FastReader(); 
+        int n = s.nextInt(); 
+        int f=0;
+        for (int i = 2; i <= n-2; i+=2) {
+                n -= i;
+                if(n%2==0){
+                f=1;
+                System.out.println("YES");
+                break;
+                }
+            
+        }
+        if(f==0)System.out.println("NO");
+    } 
+} 
