e382fc7566084cd697db0018a87fbb9d69b89433
Akshat <64137417+akshat-fsociety@users.noreply.github.com>
2020-10-31 13:33:09 +0530

    Create 1228.java

---
+++ Codeforces/codeforces-ladders/Ladder A/1228.java
@@ -0,0 +1,99 @@
+//https://codeforces.com/contest/1228/problem/A
+
+// import java.io.*;
+// import java.util.*;
+import java.io.BufferedReader; 
+import java.io.IOException; 
+import java.io.InputStreamReader; 
+// import java.util.Scanner; 
+import java.util.StringTokenizer; 
+  
+public class distinctDigit
+{ 
+    static class FastReader 
+    { 
+        BufferedReader br; 
+        StringTokenizer st; 
+  
+        public FastReader() 
+        { 
+            br = new BufferedReader(new
+                     InputStreamReader(System.in)); 
+        } 
+  
+        String next() 
+        { 
+            while (st == null || !st.hasMoreElements()) 
+            { 
+                try
+                { 
+                    st = new StringTokenizer(br.readLine()); 
+                } 
+                catch (IOException  e) 
+                { 
+                    e.printStackTrace(); 
+                } 
+            } 
+            return st.nextToken(); 
+        } 
+  
+        int nextInt() 
+        { 
+            return Integer.parseInt(next()); 
+        } 
+  
+        long nextLong() 
+        { 
+            return Long.parseLong(next()); 
+        } 
+  
+        double nextDouble() 
+        { 
+            return Double.parseDouble(next()); 
+        } 
+  
+        String nextLine() 
+        { 
+            String str = ""; 
+            try
+            { 
+                str = br.readLine(); 
+            } 
+            catch (IOException e) 
+            { 
+                e.printStackTrace(); 
+            } 
+            return str; 
+        } 
+    } 
+  
+    public static void main(String[] args) 
+    { 
+        FastReader in = new FastReader(); 
+        int l = in.nextInt();
+        int r = in.nextInt();
+        int f=0;
+        for (int i=l ; i<=r ; i++) 
+        { 
+            int num = i; 
+            boolean visited[] = new boolean[100];  
+            while (num != 0) 
+            { 
+                if (visited[num % 10]) 
+                    break; 
+       
+                visited[num%10] = true; 
+       
+                num = num/10; 
+            } 
+            if (num == 0){ 
+                f=1;
+                System.out.print(i);
+                break; 
+            }
+        } 
+        if(f==0) {
+            System.out.println(-1);
+        }
+    }
+} 
