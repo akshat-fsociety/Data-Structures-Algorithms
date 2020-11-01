ae0683f2be23b184fcedbaa49c6fae4189a45a39
Akshat <64137417+akshat-fsociety@users.noreply.github.com>
2020-11-01 16:47:37 +0530

    Create 199A.java

---
+++ Codeforces/codeforces-ladders/Ladder A/199A.java
@@ -0,0 +1,77 @@
+//https://codeforces.com/problemset/problem/199/A
+
+import java.io.*;
+import java.util.*;
+import java.io.BufferedReader; 
+import java.io.IOException; 
+import java.io.InputStreamReader; 
+// import java.util.Scanner; 
+import java.util.StringTokenizer; 
+  
+public class hexadecimal
+{ 
+    static class FastReader 
+    { 
+        BufferedReader br; 
+        StringTokenizer st; 
+  
+        public FastReader() 
+        { 
+            br = new BufferedReader(new InputStreamReader(System.in)); 
+        } 
+  
+        String next() 
+        { 
+            while (st == null || !st.hasMoreElements()) 
+            { 
+                try
+                { 
+                    st = new StringTokenizer(br.readLine()); 
+                } 
+                catch (IOException  e) 
+                { 
+                    e.printStackTrace(); 
+                } 
+            } 
+            return st.nextToken(); 
+        } 
+  
+        int nextInt() 
+        { 
+            return Integer.parseInt(next()); 
+        } 
+  
+        long nextLong() 
+        { 
+            return Long.parseLong(next()); 
+        } 
+  
+        double nextDouble() 
+        { 
+            return Double.parseDouble(next()); 
+        } 
+  
+        String nextLine() 
+        { 
+            String str = ""; 
+            try
+            { 
+                str = br.readLine(); 
+            } 
+            catch (IOException e) 
+            { 
+                e.printStackTrace(); 
+            } 
+            return str; 
+        } 
+    } 
+  
+    public static void main(String[] args) 
+    { 
+        FastReader s=new FastReader(); 
+        PrintWriter out=new PrintWriter(System.out);
+        int n = s.nextInt();
+        out.println("0 0 "+n);
+        out.close();
+    }
+} 
