745f57802b67554117c837ccd43d82c7becb3e5e
Akshat <64137417+akshat-fsociety@users.noreply.github.com>
2020-11-03 10:54:37 +0530

    Create 1433.java

---
+++ Codeforces/codeforces-ladders/Ladder A/1433.java
@@ -0,0 +1,87 @@
+//https://codeforces.com/contest/1433/problem/A
+
+import java.io.*;
+import java.util.*;
+import java.io.BufferedReader; 
+import java.io.IOException; 
+import java.io.InputStreamReader; 
+// import java.util.Scanner; 
+import java.util.StringTokenizer; 
+  
+public class boringApartment
+{ 
+    static class FastReader 
+    { 
+        BufferedReader br; 
+        StringTokenizer st; 
+  
+        public FastReader() 
+        { 
+            br = new BufferedReader(new
+                     InputStreamReader(System.in)); 
+        } 
+  
+        String next() 
+        { 
+            while (st == null || !st.hasMoreElements()) 
+            { 
+                try
+                { 
+                    st = new StringTokenizer(br.readLine()); 
+                } 
+                catch (IOException  e) 
+                { 
+                    e.printStackTrace(); 
+                } 
+            } 
+            return st.nextToken(); 
+        } 
+  
+        int nextInt() 
+        { 
+            return Integer.parseInt(next()); 
+        } 
+  
+        long nextLong() 
+        { 
+            return Long.parseLong(next()); 
+        } 
+  
+        double nextDouble() 
+        { 
+            return Double.parseDouble(next()); 
+        } 
+  
+        String nextLine() 
+        { 
+            String str = ""; 
+            try
+            { 
+                str = br.readLine(); 
+            } 
+            catch (IOException e) 
+            { 
+                e.printStackTrace(); 
+            } 
+            return str; 
+        } 
+    } 
+  
+    public static void main(String[] args) 
+    { 
+        FastReader s=new FastReader(); 
+        int t = s.nextInt();
+        while(t--!=0){
+            int n = s.nextInt();
+            int j=n%10;
+            int val = 10*(j-1);
+            if(n<=9)val++;
+            else if(n>=11 && n<=99)val+=3;
+            else if(n>=111 && n<=999)val+=6;
+            else if(n>=1111 && n<=9999)val+=10;
+            System.out.println(val);
+        }
+        
+    }
+} 
+
