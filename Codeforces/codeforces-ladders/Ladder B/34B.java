bf5ee4562a5afc8bc26e98eaba3473651d4dea60
Akshat <64137417+akshat-fsociety@users.noreply.github.com>
2020-11-03 12:00:12 +0530

    Create 34B.java

---
+++ Codeforces/codeforces-ladders/Ladder B/34B.java
@@ -0,0 +1,94 @@
+//https://codeforces.com/problemset/problem/34/B
+
+import java.io.*;
+import java.util.*;
+import java.io.BufferedReader; 
+import java.io.IOException; 
+import java.io.InputStreamReader; 
+// import java.util.Scanner; 
+import java.util.StringTokenizer; 
+  
+public class sale
+{ 
+    static class FastReader 
+    { 
+        BufferedReader br; 
+        StringTokenizer st; 
+  
+        public FastReader() 
+        { 
+            br = new BufferedReader(new
+                     InputStreamReader(System.in)); 
+        } 
+  
+        String next() 
+        { 
+            while (st == null || !st.hasMoreElements()) 
+            { 
+                try
+                { 
+                    st = new StringTokenizer(br.readLine()); 
+                } 
+                catch (IOException  e) 
+                { 
+                    e.printStackTrace(); 
+                } 
+            } 
+            return st.nextToken(); 
+        } 
+  
+        int nextInt() 
+        { 
+            return Integer.parseInt(next()); 
+        } 
+  
+        long nextLong() 
+        { 
+            return Long.parseLong(next()); 
+        } 
+  
+        double nextDouble() 
+        { 
+            return Double.parseDouble(next()); 
+        } 
+  
+        String nextLine() 
+        { 
+            String str = ""; 
+            try
+            { 
+                str = br.readLine(); 
+            } 
+            catch (IOException e) 
+            { 
+                e.printStackTrace(); 
+            } 
+            return str; 
+        } 
+    } 
+  
+    public static void main(String[] args) 
+    { 
+        FastReader s=new FastReader(); 
+        int n = s.nextInt();
+        int m = s.nextInt();
+        int a[] = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = s.nextInt();
+        }
+        Arrays.sort(a);
+        int sum=0;
+        int min = Integer.MAX_VALUE;
+        if(a[0]>0)System.out.println(0);
+        else{
+        for (int i = 0; i < m; i++) {
+            sum+=a[i];
+            if(sum<min){
+                min=sum;
+            }
+        }
+        System.out.println(Math.abs(min));
+        }
+    }
+} 
+
