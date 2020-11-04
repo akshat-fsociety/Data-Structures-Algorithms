17d579058aa38f43746bd2c6e6236aca327f961f
Akshat <64137417+akshat-fsociety@users.noreply.github.com>
2020-11-04 16:33:54 +0530

    Create 260A.java

---
+++ Codeforces/codeforces-ladders/Ladder A/260A.java
@@ -0,0 +1,94 @@
+//https://codeforces.com/problemset/problem/260/A
+
+import java.io.*;
+// import java.util.*;
+import java.io.BufferedReader; 
+import java.io.IOException; 
+import java.io.InputStreamReader; 
+// import java.util.Scanner; 
+import java.util.StringTokenizer; 
+  
+public class adding
+{ 
+    static class FastReader 
+    { 
+        BufferedReader br; 
+        StringTokenizer st; 
+  
+        public FastReader() 
+        { 
+            br = new BufferedReader(new InputStreamReader(System.in)); 
+        } 
+  
+        String next() 
+        { 
+            while (st == null || !st.hasMoreElements()) 
+            { 
+                try
+                { 
+                    st = new StringTokenizer(br.readLine()); 
+                } 
+                catch (IOException  e) 
+                { 
+                    e.printStackTrace(); 
+                } 
+            } 
+            return st.nextToken(); 
+        } 
+  
+        int nextInt() 
+        { 
+            return Integer.parseInt(next()); 
+        } 
+  
+        long nextLong() 
+        { 
+            return Long.parseLong(next()); 
+        } 
+  
+        double nextDouble() 
+        { 
+            return Double.parseDouble(next()); 
+        } 
+  
+        String nextLine() 
+        { 
+            String str = ""; 
+            try
+            { 
+                str = br.readLine(); 
+            } 
+            catch (IOException e) 
+            { 
+                e.printStackTrace(); 
+            } 
+            return str; 
+        } 
+    } 
+  
+    public static void main(String[] args) 
+    { 
+        FastReader s=new FastReader(); 
+        int a = s.nextInt();
+        int b = s.nextInt();
+        int n = s.nextInt();
+        long f=0l;
+        long num=a;
+        for (int i = 0; i <= 9; i++) {
+                long x = (num*10)+i;
+                if(x%b==0){
+                    f=1;
+                    num=x;
+                    break;
+            }
+        }
+        if(f==1){
+            System.out.print(num);
+            for (int i = 1; i <= n-1; i++) {
+                System.out.print(0);
+            }
+        }
+        else System.out.println(-1);
+        
+    }
+} 
