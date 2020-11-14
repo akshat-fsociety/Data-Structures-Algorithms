1bf0ee13131650cb7190ab705e8db88e6774ac2a
Akshat <64137417+akshat-fsociety@users.noreply.github.com>
2020-11-14 11:42:41 +0530

    Create 1438.java

---
+++ Codeforces/codeforces-ladders/Ladder A/1438.java
@@ -0,0 +1,83 @@
+//https://codeforces.com/contest/1438/problem/A
+
+import java.io.*;
+import java.util.*;
+import java.io.BufferedReader; 
+import java.io.IOException; 
+import java.io.InputStreamReader; 
+// import java.util.Scanner; 
+import java.util.StringTokenizer; 
+  
+public class specific 
+{ 
+    static class FastReader 
+    { 
+        BufferedReader br; 
+        StringTokenizer st; 
+  
+        public FastReader() 
+        { 
+            br = new BufferedReader(new
+                     InputStreamReader(System.in)); 
+        } 
+  
+        String next() 
+        { 
+            while (st == null || !st.hasMoreElements()) 
+            { 
+                try
+                { 
+                    st = new StringTokenizer(br.readLine()); 
+                } 
+                catch (IOException  e) 
+                { 
+                    e.printStackTrace(); 
+                } 
+            } 
+            return st.nextToken(); 
+        } 
+  
+        int nextInt() 
+        { 
+            return Integer.parseInt(next()); 
+        } 
+  
+        long nextLong() 
+        { 
+            return Long.parseLong(next()); 
+        } 
+  
+        double nextDouble() 
+        { 
+            return Double.parseDouble(next()); 
+        } 
+  
+        String nextLine() 
+        { 
+            String str = ""; 
+            try
+            { 
+                str = br.readLine(); 
+            } 
+            catch (IOException e) 
+            { 
+                e.printStackTrace(); 
+            } 
+            return str; 
+        } 
+    } 
+  
+    public static void main(String[] args) 
+    { 
+        FastReader s=new FastReader(); 
+        int t = s.nextInt(); 
+        while (t--!=0) 
+        { 
+            int n = s.nextInt();
+            for (int i = 1; i <= n; i++) {
+                System.out.print(1+" ");               
+            }
+            System.out.println();
+        } 
+    } 
+} 
