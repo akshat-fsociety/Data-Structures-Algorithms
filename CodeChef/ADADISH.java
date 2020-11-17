ab0f261e77eb08455c8237b88ad4d6b277059ca0
Akshat <64137417+akshat-fsociety@users.noreply.github.com>
2020-11-17 19:04:00 +0530

    Update ADADISH.java

--- CodeChef/ADADISH.java
+++ CodeChef/ADADISH.java
@@ -1,29 +1,15 @@
 //https://www.codechef.com/NOV20B/problems/ADADISH
 
 /* package codechef; // don't place package name! */
+/* package codechef; // don't place package name! */
 
 import java.util.*;
 import java.lang.*;
 import java.io.*;
 
 /* Name of the class has to be "Main" only if the class is public. */
-class Codechef
+class adadish
 {
-    public static int findRecur(int a[], int i, int sum, int sumTot){
-	    if(i==0){
-	    int x = Math.abs((sumTot-sum)-sum);
-	    return x;
-	    }
-	    return Math.min(findRecur(a, i-1, sum+a[i-1], sumTot), findRecur(a, i-1, sum, sumTot));
-	}
-	
-	public static int min(int a[], int n){
-	    int sumTot = 0;
-	    for(int i=0; i<n; i++)sumTot += a[i];
-	    
-	    return findRecur(a, n, 0, sumTot);
-	}
-	
 	public static void main (String[] args) throws java.lang.Exception
 	{
 		Scanner sc = new Scanner(System.in);
@@ -32,24 +18,71 @@ class Codechef
 		t = sc.nextInt();
 		while(t--!=0){
 		    int n = sc.nextInt();
-            int a[] = new int[n];
-            double s=0;
-            for (int i = 0; i < n; i++) {
-                a[i] = sc.nextInt();
-                s+=a[i];
-            }
-            Arrays.sort(a);
-            if(n==1)System.out.println(a[0]);
-            else if(n==2)System.out.println(a[1]);
-            else{
-                int d = min(a,n);
-                double h = s/2;
-                double diffh = d/2;
-                h -= diffh;
-                h += d;
-                System.out.println((int)h);
-            }
+		    int a[] = new int[n];
+		    int des[] = new int[n];
+		    int b1=0, b2=0;
+		    for (int i=0;i<n ;i++ )a[i] = sc.nextInt();
+		    des = sortdes(a);
+		    for (int i=0;i<n ;i++ ){
+		        if(b1<b2)b1+=des[i];
+		        else b2+=des[i];
+		    }
+		    System.out.println(Math.max(b1,b2));
 		}
 	}
+	public static ArrayList<Long> sieve(int n) 
+    { 
+        boolean prime[] = new boolean[n+1]; 
+        ArrayList<Long>prime1 = new ArrayList<>();
+        for(int i=0;i<n;i++) 
+            prime[i] = true; 
+          
+        for(int p = 2; p*p <=n; p++) 
+        {
+            if(prime[p] == true) 
+            { 
+                for(int i = p*p; i <= n; i += p) 
+                    prime[i] = false; 
+            } 
+        }
+        for(int i = 2; i <= n; i++) 
+        { 
+            if(prime[i] == true){ 
+                prime1.add((long)i);
+            }
+        } 
+        return prime1;
+    }
+    public static int[] sievearr(int n) 
+    { 
+        boolean prime[] = new boolean[n+1]; 
+        int primex[] = new int[n+1];
+        for(int i=0;i<n;i++) 
+            prime[i] = true; 
+          
+        for(int p = 2; p*p <=n; p++) 
+        {
+            if(prime[p] == true) 
+            { 
+                for(int i = p*p; i <= n; i += p) 
+                    prime[i] = false; 
+            } 
+        }
+        for(int i = 2; i <= n; i++) 
+        { 
+            if(prime[i] == true){ 
+                primex[i]=i;
+            }
+        } 
+        return primex;
+    }
+    public static int[] sortdes(int a[]){
+        Arrays.sort(a);
+        int des[] = new int[a.length];
+        int ind=a.length-1;
+        for(int e:a){
+            des[ind--]=e;
+        }
+        return des;
+    }
 }
-
