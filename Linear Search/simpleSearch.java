40de77633d55c833c0c1d390d850b5174640f342
Akshat <64137417+akshat-fsociety@users.noreply.github.com>
2020-11-19 11:38:41 +0530

    Update simpleSearch.java

--- Linear Search/simpleSearch.java
+++ Linear Search/simpleSearch.java
@@ -2,20 +2,21 @@
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Scanner;
 import java.util.*;
 
 class TestClass {
     public static void main(String args[] ) throws Exception {
         Scanner s = new Scanner(System.in);
         int n = s.nextInt();
-        int a[] = new int[n];
-        for(int i=0; i<n; i++)a[i] = s.nextInt();
+        ArrayList<Integer> a = new ArrayList<>(n);
+        for(int i=0; i<n; i++)a.add(s.nextInt());
         int k = s.nextInt();
         for(int i=0; i<n; i++){
-            if(a[i]==k){
+            if(a.get(i)==k){
                 System.out.println(i);
+                break;
             }
         }
-
     }
 }
