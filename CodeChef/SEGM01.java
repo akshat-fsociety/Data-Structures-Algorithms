5271de418954f6693dfd75cb4c01a1355d6c6ce8
Akshat <64137417+akshat-fsociety@users.noreply.github.com>
2020-11-20 19:32:39 +0530

    Create SEGM01.java

---
+++ CodeChef/SEGM01.java
@@ -0,0 +1,97 @@
+//https://www.codechef.com/problems/SEGM01
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		int t=0; 
+		if(sc.hasNextInt())
+		t = sc.nextInt();
+		while(t--!=0){
+		    String s = sc.next();
+		    int n = s.length();
+		    int first=0, second=0;
+		    for(int i=0; i<n; i++){
+		        if(s.charAt(i)=='1'){
+		            first = i; 
+		            break;
+		        }
+		    }
+		    for(int i=n-1; i>=0; i--){
+		        if(s.charAt(i)=='1'){
+		            second = i; 
+		            break;
+		        }
+		    }
+		    int f=0;
+		    for (int i=first;i<=second;i++ ){
+		        if(s.charAt(i)=='0'){
+		            f=1;
+		            System.out.println("NO");
+		            break;
+		        }
+		    }
+		    if(f==0)System.out.println("YES");
+		}
+	}
+	public static ArrayList<Long> sieve(int n) 
+    { 
+        boolean prime[] = new boolean[n+1]; 
+        ArrayList<Long>prime1 = new ArrayList<>();
+        for(int i=0;i<n;i++) 
+            prime[i] = true; 
+          
+        for(int p = 2; p*p <=n; p++) 
+        {
+            if(prime[p] == true) 
+            { 
+                for(int i = p*p; i <= n; i += p) 
+                    prime[i] = false; 
+            } 
+        }
+        for(int i = 2; i <= n; i++) 
+        { 
+            if(prime[i] == true){ 
+                prime1.add((long)i);
+            }
+        } 
+        return prime1;
+    }
+    public static int[] sievearr(int n) 
+    { 
+        boolean prime[] = new boolean[n+1]; 
+        int primex[] = new int[n+1];
+        for(int i=0;i<n;i++) 
+            prime[i] = true; 
+          
+        for(int p = 2; p*p <=n; p++) 
+        {
+            if(prime[p] == true) 
+            { 
+                for(int i = p*p; i <= n; i += p) 
+                    prime[i] = false; 
+            } 
+        }
+        for(int i = 2; i <= n; i++) 
+        { 
+            if(prime[i] == true){ 
+                primex[i]=i;
+            }
+        } 
+        return primex;
+    }
+    public static int[] sortdes(int a[]){
+        Arrays.sort(a);
+        int des[] = new int[a.length];
+        int ind=a.length-1;
+        for(int e:a){
+            des[ind--]=e;
+        }
+        return des;
+    }
+}
