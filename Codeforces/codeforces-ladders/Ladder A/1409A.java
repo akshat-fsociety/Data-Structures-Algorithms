bffcb37acb51e7dab5d0a43787007faf1c62bb8c
Akshat <64137417+akshat-fsociety@users.noreply.github.com>
2020-11-25 16:38:39 +0530

    Create 1409A.java

---
+++ Codeforces/codeforces-ladders/Ladder A/1409A.java
@@ -0,0 +1,86 @@
+//https://codeforces.com/contest/1409/problem/A
+
+import java.io.*;
+import java.util.*;
+import java.io.BufferedReader; 
+import java.io.IOException; 
+import java.io.InputStreamReader; 
+// import java.util.Scanner; 
+import java.util.StringTokenizer; 
+  
+public class two
+{ 
+    static class FastReader 
+    { 
+        BufferedReader br; 
+        StringTokenizer st; 
+  
+        public FastReader() 
+        { 
+            br = new BufferedReader(new
+                     InputStreamReader(System.in)); 
+        } 
+  
+        String next() 
+        { 
+            while (st == null || !st.hasMoreElements()) 
+            { 
+                try
+                { 
+                    st = new StringTokenizer(br.readLine()); 
+                } 
+                catch (IOException  e) 
+                { 
+                    e.printStackTrace(); 
+                } 
+            } 
+            return st.nextToken(); 
+        } 
+  
+        int nextInt() 
+        { 
+            return Integer.parseInt(next()); 
+        } 
+  
+        long nextLong() 
+        { 
+            return Long.parseLong(next()); 
+        } 
+  
+        double nextDouble() 
+        { 
+            return Double.parseDouble(next()); 
+        } 
+  
+        String nextLine() 
+        { 
+            String str = ""; 
+            try
+            { 
+                str = br.readLine(); 
+            } 
+            catch (IOException e) 
+            { 
+                e.printStackTrace(); 
+            } 
+            return str; 
+        } 
+    } 
+  
+    public static void main(String[] args) 
+    { 
+        FastReader s=new FastReader(); 
+        long t = s.nextLong();
+        while(t--!=0){
+            long c=0;
+            long a = s.nextLong();
+            long b = s.nextLong();
+            long diff = Math.abs(b-a);
+            c = diff/10;
+            long x = diff%10;
+            if(x>=1 && x<=9) 
+            System.out.println(++c);
+            else  System.out.println(c);
+        }
+    } 
+} 
