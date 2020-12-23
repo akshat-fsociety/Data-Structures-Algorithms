6a172a264da0c7b59a9c8f9cc2b8081efafecb24
Akshat <64137417+akshat-fsociety@users.noreply.github.com>
2020-12-23 20:35:29 +0530

    Create 1465B.java

---
+++ Codeforces/codeforces-ladders/Ladder B/1465B.java
@@ -0,0 +1,92 @@
+//https://codeforces.com/contest/1465/problem/B
+
+// import java.io.*;
+import java.util.*;
+import java.io.BufferedReader; 
+import java.io.IOException; 
+import java.io.InputStreamReader; 
+// import java.util.Scanner; 
+import java.util.StringTokenizer; 
+  
+public class fairnum
+{ 
+    static class FastReader 
+    { 
+        BufferedReader br; 
+        StringTokenizer st; 
+  
+        public FastReader() 
+        { 
+            br = new BufferedReader(new
+                     InputStreamReader(System.in)); 
+        } 
+  
+        String next() 
+        { 
+            while (st == null || !st.hasMoreElements()) 
+            { 
+                try
+                { 
+                    st = new StringTokenizer(br.readLine()); 
+                } 
+                catch (IOException  e) 
+                { 
+                    e.printStackTrace(); 
+                } 
+            } 
+            return st.nextToken(); 
+        } 
+  
+        int nextInt() 
+        { 
+            return Integer.parseInt(next()); 
+        } 
+  
+        long nextLong() 
+        { 
+            return Long.parseLong(next()); 
+        } 
+  
+        double nextDouble() 
+        { 
+            return Double.parseDouble(next()); 
+        } 
+  
+        String nextLine() 
+        { 
+            String str = ""; 
+            try
+            { 
+                str = br.readLine(); 
+            } 
+            catch (IOException e) 
+            { 
+                e.printStackTrace(); 
+            } 
+            return str; 
+        } 
+    } 
+  
+    public static void main(String[] args) 
+    { 
+        FastReader s=new FastReader(); 
+        long t = s.nextLong();
+        while(t--!=0){
+            long n=s.nextLong(); 
+            while(!div(n)){
+                n++;
+            }
+            System.out.println(n);
+        }
+    }
+    public static boolean div(long n){
+        long x = n;
+        while(n>0){
+            if(n%10!=0 && x%(n%10)!=0){
+                return false;
+            }
+            n/=10;
+        }
+        return true;
+    }
+} 
