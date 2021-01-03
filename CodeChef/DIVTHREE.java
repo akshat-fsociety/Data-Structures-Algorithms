ee67f489139e4bbff64920c05ece86167865f4c4
Akshat <64137417+akshat-fsociety@users.noreply.github.com>
2021-01-03 11:38:18 +0530

    Create DIVTHREE.java

---
+++ CodeChef/DIVTHREE.java
@@ -0,0 +1,82 @@
+// https://www.codechef.com/JAN21C/problems/DIVTHREE
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		int t=0; 
+		if(sc.hasNextInt())
+		t = sc.nextInt();
+		while(t--!=0){
+		    int n = sc.nextInt();
+		    long k = sc.nextLong();
+		    long d = sc.nextLong();
+		    long a[] = new long[n];
+		    long sum = 0l;
+		    for(int i=0;i<n; i++)a[i] = sc.nextLong();
+		    for(int i=0;i<n; i++)sum+=a[i];
+		    long days = sum/k;
+		    System.out.println(Math.min(days,d));
+		}
+	}
+	public static ArrayList<Long> sieve(int n) 
+    { 
+        boolean prime[] = new boolean[n+1]; 
+        ArrayList<Long>prime1 = new ArrayList<>();
+        for(int i=0;i<n;i++) 
+            prime[i] = true; 
+          
+        for(int p = 2; p*p <=n; p++) 
+        {
+            if(prime[p] == true) 
+            { 
+                for(int i = p*p; i <= n; i += p) 
+                    prime[i] = false; 
+            } 
+        }
+        for(int i = 2; i <= n; i++) 
+        { 
+            if(prime[i] == true){ 
+                prime1.add((long)i);
+            }
+        } 
+        return prime1;
+    }
+    public static int[] sievearr(int n) 
+    { 
+        boolean prime[] = new boolean[n+1]; 
+        int primex[] = new int[n+1];
+        for(int i=0;i<n;i++) 
+            prime[i] = true; 
+          
+        for(int p = 2; p*p <=n; p++) 
+        {
+            if(prime[p] == true) 
+            { 
+                for(int i = p*p; i <= n; i += p) 
+                    prime[i] = false; 
+            } 
+        }
+        for(int i = 2; i <= n; i++) 
+        { 
+            if(prime[i] == true){ 
+                primex[i]=i;
+            }
+        } 
+        return primex;
+    }
+    public static int[] sortdes(int a[]){
+        Arrays.sort(a);
+        int des[] = new int[a.length];
+        int ind=a.length-1;
+        for(int e:a){
+            des[ind--]=e;
+        }
+        return des;
+    }
+}
