b388708b6d8d959a9d59ce5d6388f780ab5ed4cc
Akshat Srivastava <64137417+akshat-fsociety@users.noreply.github.com>
2021-08-14 20:34:32 +0530

    Create D3_P1_SearchMatrix.java

---
+++ StriverSDE/Arrays/D3_P1_SearchMatrix.java
@@ -0,0 +1,17 @@
+// https://leetcode.com/problems/search-a-2d-matrix/
+// Advance type of Binary search
+
+class Solution {
+    public boolean searchMatrix(int[][] m, int target) {
+        int i = 0, j = m[0].length-1; 
+        while(i < m.length && j>=0){
+            if(m[i][j]==target) return true;
+            else if(m[i][j]>target){
+                j--;
+            }else{
+                i++;
+            }
+        }
+        return false;
+    }
+}
