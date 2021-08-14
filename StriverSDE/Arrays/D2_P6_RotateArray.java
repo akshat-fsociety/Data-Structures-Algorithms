0e166b3bf082d6f6c8cae6ce15367aa642b16b0b
Akshat Srivastava <64137417+akshat-fsociety@users.noreply.github.com>
2021-08-14 19:30:47 +0530

    Create D2_P6_RotateArray.java

---
+++ StriverSDE/Arrays/D2_P6_RotateArray.java
@@ -0,0 +1,32 @@
+// https://leetcode.com/problems/rotate-image/
+
+class Solution {
+    public void rotate(int[][] a) {
+        
+      // TRANSPOSE
+        for(int r=0; r<a.length; r++){
+            for(int c=r; c<a[0].length; c++){
+                int t = a[r][c];
+                a[r][c] = a[c][r];
+                a[c][r] = t;
+            }
+        }
+      
+      // REVERSE EACH ROW
+        for(int r=0; r<a.length; r++){
+            for(int c=0; c<a[0].length/2; c++){
+                int t = a[r][c];
+                a[r][c] = a[r][a.length-1-c];
+                a[r][a.length-1-c] = t;
+            }
+        }
+      
+      //DISPLAY
+        for(int r=0; r<a.length; r++){
+            for(int c=0; c<a[0].length; c++){
+                System.out.print(a[r][c]+" ");
+            }
+            System.out.println();
+        }
+    }
+}
