3dbebd0c93a3101610c079d3dff0d0147068f9ad
Akshat Srivastava <64137417+akshat-fsociety@users.noreply.github.com>
2021-08-19 20:43:33 +0530

    Create Duplicate_Number.java

---
+++ DSA CRACKER 450/Array/Duplicate_Number.java
@@ -0,0 +1,17 @@
+// https://leetcode.com/problems/find-the-duplicate-number/
+
+class Solution {
+    public int findDuplicate(int[] nums) {
+        HashMap<Integer,Boolean>hm = new HashMap<>();
+        int res =0;
+        for(int i=0; i<nums.length; i++){
+            if(hm.containsKey(nums[i])){
+                res=nums[i];
+                break;
+            }else{
+                hm.put(nums[i],true);
+            }
+        }
+        return res;
+    }
+}
