f36404f6af45baf44281e87356f0f9c68b4b1e2c
Akshat Srivastava <64137417+akshat-fsociety@users.noreply.github.com>
2021-08-27 14:08:48 +0530

    Update Longest_repeating_subsequence.java

--- DSA CRACKER 450/String/Longest_repeating_subsequence.java
+++ DSA CRACKER 450/String/Longest_repeating_subsequence.java
@@ -14,7 +14,7 @@ class Solution
         }
         for(int i=1; i<n+1; i++){
             for(int j=1; j<n+1; j++){
-                if(s.charAt(i-1)==s.charAt(j-1) && i!=j){
+                if(s.charAt(i-1)==s.charAt(j-1) && i!=j){    // i!=j (used for not same index matching) ----> catch
                     dp[i][j] = 1+dp[i-1][j-1];
                 }else{
                     dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
