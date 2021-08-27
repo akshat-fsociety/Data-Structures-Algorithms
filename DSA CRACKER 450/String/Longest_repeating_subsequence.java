03352c3b3b50145107120563860e0f3bcaa9d0bb
Akshat Srivastava <64137417+akshat-fsociety@users.noreply.github.com>
2021-08-27 14:07:36 +0530

    Create Longest_repeating_subsequence.java

---
+++ DSA CRACKER 450/String/Longest_repeating_subsequence.java
@@ -0,0 +1,26 @@
+// https://practice.geeksforgeeks.org/problems/longest-repeating-subsequence2004/1
+
+class Solution
+{
+    public int LongestRepeatingSubsequence(String s)
+    {
+        // code here
+        int n = s.length();
+        int dp[][] = new int[n+1][n+1];
+        for(int i=0; i<n+1; i++){
+            for(int j=0; j<n+1; j++){
+                if(i==0 || j==0)dp[i][j] = 0;
+            }
+        }
+        for(int i=1; i<n+1; i++){
+            for(int j=1; j<n+1; j++){
+                if(s.charAt(i-1)==s.charAt(j-1) && i!=j){
+                    dp[i][j] = 1+dp[i-1][j-1];
+                }else{
+                    dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
+                }
+            }
+        }
+        return dp[n][n];
+    }
+}
