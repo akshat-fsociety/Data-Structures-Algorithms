cb01a40d5d75b61d877532ea8635816b4e7d0bf7
Akshat Srivastava <64137417+akshat-fsociety@users.noreply.github.com>
2021-08-28 13:28:29 +0530

    Create D5_P4_Remove_nth_node_from_end.java

---
+++ StriverSDE/Linked List/D5_P4_Remove_nth_node_from_end.java
@@ -0,0 +1,19 @@
+// https://leetcode.com/problems/remove-nth-node-from-end-of-list/
+
+class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode start = new ListNode();
+        start.next = head;
+        ListNode fast = start;
+        ListNode slow = start;
+        for(int i=1; i<=n; i++){
+            fast = fast.next;
+        }
+        while(fast.next!=null){
+            fast = fast.next;
+            slow = slow.next;
+        }
+        slow.next = slow.next.next;
+        return start.next;
+    }
+}
