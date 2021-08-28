bfa3a2f7384fe501dae52877665716d6e55774b4
Akshat Srivastava <64137417+akshat-fsociety@users.noreply.github.com>
2021-08-28 13:42:19 +0530

    Create D5_P5_Delete_a_node.java

---
+++ StriverSDE/Linked List/D5_P5_Delete_a_node.java
@@ -0,0 +1,8 @@
+// https://leetcode.com/problems/delete-node-in-a-linked-list/
+
+class Solution {
+    public void deleteNode(ListNode node) {
+        node.val = node.next.val;
+        node.next = node.next.next;
+    }
+}
