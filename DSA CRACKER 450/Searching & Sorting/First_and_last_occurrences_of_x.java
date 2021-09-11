5fd7ade5b01f4f41f0b23e5d1bc4f82eca4b9470
Akshat Srivastava <64137417+akshat-fsociety@users.noreply.github.com>
2021-09-11 20:19:43 +0530

    Create First_and_last_occurrences_of_x.java

---
+++ DSA CRACKER 450/Searching & Sorting/First_and_last_occurrences_of_x.java
@@ -0,0 +1,41 @@
+class GFG
+{
+    ArrayList<Long> find(long a[], int n, int x)
+    {
+        // code here
+        ArrayList<Long> ans = new ArrayList<>();
+        int start = 0, end = n-1;
+        long first=-1, last=-1;
+      
+      // This Binary Search will find the first Occurrence of x
+        while(start<=end){
+            int mid = start+(end-start)/2;
+            if(x==a[mid]){
+                first = mid;
+                end = mid-1;
+            }else if(x>a[mid]){
+                start = mid+1;
+            }else{
+                end = mid-1;
+            }
+        }
+      
+      // This Binary Search will find the last Occurrence of x
+        start = 0; end = n-1;
+        while(start<=end){
+            int mid = start+(end-start)/2;
+            if(x==a[mid]){
+                last = mid;
+                start = mid+1;
+            }else if(x>a[mid]){
+                start = mid+1;
+            }else{
+                end = mid-1;
+            }
+        }
+        ans.add(first);
+        ans.add(last);
+        return ans;
+    }
+}
+
