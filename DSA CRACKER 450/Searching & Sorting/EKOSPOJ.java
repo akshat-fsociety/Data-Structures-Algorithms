b0ecdc7fa8e3aed34a815799aca302b244ba3107
Akshat Srivastava <64137417+akshat-fsociety@users.noreply.github.com>
2021-09-22 18:58:36 +0530

    Create EKOSPOJ.java

---
+++ DSA CRACKER 450/Searching & Sorting/EKOSPOJ.java
@@ -0,0 +1,236 @@
+// https://www.spoj.com/problems/EKO/
+
+import java.io.*; 
+import java.util.*; 
+import java.text.*; 
+import java.math.*; 
+import java.util.regex.*; 
+import java.lang.*; 
+ 
+class Main { 
+	static long mod = 1000000007; 
+ 
+	public static void main(String[] args) throws Exception { 
+		InputReader in = new InputReader(System.in); 
+		PrintWriter pw = new PrintWriter(System.out); 
+		long n = in.readLong();
+		long m = in.readLong();
+		long a[] = new long[(int)n];
+		for(int i=0; i<n; i++){
+		    a[i] = in.readLong();
+		}
+		long min = Long.MAX_VALUE;
+		long max = Long.MIN_VALUE;
+		for(int i=0; i<n; i++){
+		    min = Math.min(a[i],min);
+		    max = Math.max(a[i],max);
+		}
+		long low = min, high = max, res = 0;
+        while(low<=high){
+            long mid = low+(high-low)/2;
+            if(canCut(a,n,m,mid)){
+                low = mid+1;
+                res = mid;
+            }else{
+                high = mid-1;
+            }
+        }
+        System.out.println(res);
+	}
+ static boolean canCut(long a[], long n, long m, long mid){
+	    long s = 0;
+	    for(int i=0; i<n; i++){
+	        if(a[i]>=mid){
+	            s+=a[i]-mid;
+	        }
+	    }
+	    if(s>=m)return true;
+	    else return false;
+	}
+ 
+	public static long gcd(long x, long y) { 
+		if (x % y == 0) 
+			return y; 
+		else 
+			return gcd(y, x % y); 
+	} 
+ 
+	public static long pow(long n, long p, long m) { 
+		long result = 1; 
+		if (p == 0) 
+			return 1; 
+		if (p == 1) 
+			return n; 
+		while (p != 0) { 
+			if (p % 2 == 1) 
+				result *= n; 
+			if (result >= m) 
+				result %= m; 
+			p >>= 1; 
+			n *= n; 
+			if (n >= m) 
+				n %= m; 
+		} 
+		return result; 
+	} 
+ 
+	static class Pair implements Comparable<Pair> { 
+		int a, b; 
+ 
+		Pair(int a, int b) { 
+			this.a = a; 
+			this.b = b; 
+		} 
+ 
+		public int compareTo(Pair o) { 
+			// TODO Auto-generated method stub 
+			if (this.a != o.a) 
+				return Integer.compare(this.a, o.a); 
+			else 
+				return Integer.compare(this.b, o.b); 
+			// return 0; 
+		} 
+ 
+		public boolean equals(Object o) { 
+			if (o instanceof Pair) { 
+				Pair p = (Pair) o; 
+				return p.a == a && p.b == b; 
+			} 
+			return false; 
+		} 
+ 
+		public int hashCode() { 
+			return new Integer(a).hashCode() * 31 + new Integer(b).hashCode(); 
+		} 
+	} 
+ 
+	static class InputReader { 
+		private InputStream stream; 
+		private byte[] buf = new byte[1024]; 
+		private int curChar; 
+		private int numChars; 
+		private SpaceCharFilter filter; 
+ 
+		public InputReader(InputStream stream) { 
+			this.stream = stream; 
+		} 
+ 
+		public int read() { 
+			if (numChars == -1) 
+				throw new InputMismatchException(); 
+			if (curChar >= numChars) { 
+				curChar = 0; 
+				try { 
+					numChars = stream.read(buf); 
+				} catch (IOException e) { 
+					throw new InputMismatchException(); 
+				} 
+				if (numChars <= 0) 
+					return -1; 
+			} 
+			return buf[curChar++]; 
+		} 
+ 
+		public int readInt() { 
+			int c = read(); 
+			while (isSpaceChar(c)) 
+				c = read(); 
+			int sgn = 1; 
+			if (c == '-') { 
+				sgn = -1; 
+				c = read(); 
+			} 
+			int res = 0; 
+			do { 
+				if (c < '0' || c > '9') 
+					throw new InputMismatchException(); 
+				res *= 10; 
+				res += c - '0'; 
+				c = read(); 
+			} while (!isSpaceChar(c)); 
+			return res * sgn; 
+		} 
+ 
+		public String readString() { 
+			int c = read(); 
+			while (isSpaceChar(c)) 
+				c = read(); 
+			StringBuilder res = new StringBuilder(); 
+			do { 
+				res.appendCodePoint(c); 
+				c = read(); 
+			} while (!isSpaceChar(c)); 
+			return res.toString(); 
+		} 
+ 
+		public double readDouble() { 
+			int c = read(); 
+			while (isSpaceChar(c)) 
+				c = read(); 
+			int sgn = 1; 
+			if (c == '-') { 
+				sgn = -1; 
+				c = read(); 
+			} 
+			double res = 0; 
+			while (!isSpaceChar(c) && c != '.') { 
+				if (c == 'e' || c == 'E') 
+					return res * Math.pow(10, readInt()); 
+				if (c < '0' || c > '9') 
+					throw new InputMismatchException(); 
+				res *= 10; 
+				res += c - '0'; 
+				c = read(); 
+			} 
+			if (c == '.') { 
+				c = read(); 
+				double m = 1; 
+				while (!isSpaceChar(c)) { 
+					if (c == 'e' || c == 'E') 
+						return res * Math.pow(10, readInt()); 
+					if (c < '0' || c > '9') 
+						throw new InputMismatchException(); 
+					m /= 10; 
+					res += (c - '0') * m; 
+					c = read(); 
+				} 
+			} 
+			return res * sgn; 
+		} 
+ 
+		public long readLong() { 
+			int c = read(); 
+			while (isSpaceChar(c)) 
+				c = read(); 
+			int sgn = 1; 
+			if (c == '-') { 
+				sgn = -1; 
+				c = read(); 
+			} 
+			long res = 0; 
+			do { 
+				if (c < '0' || c > '9') 
+					throw new InputMismatchException(); 
+				res *= 10; 
+				res += c - '0'; 
+				c = read(); 
+			} while (!isSpaceChar(c)); 
+			return res * sgn; 
+		} 
+ 
+		public boolean isSpaceChar(int c) { 
+			if (filter != null) 
+				return filter.isSpaceChar(c); 
+			return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1; 
+		} 
+ 
+		public String next() { 
+			return readString(); 
+		} 
+ 
+		public interface SpaceCharFilter { 
+			public boolean isSpaceChar(int ch); 
+		} 
+	} 
+ 
+}
